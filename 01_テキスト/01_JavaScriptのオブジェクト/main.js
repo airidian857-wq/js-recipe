8ac4486126fe3a92465ae08414bd39972ebee56c
airidian857-wq <airidian857@gmail.com>
2025-10-10 19:30:26 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -10,17 +10,36 @@ const hikakin = {
   sayHello: function () {
     console.log("ブンブンハロー YouTube")
   },
-}
-
-// TRY: 練習問題１
+}// TRY: 練習問題１
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
-
-// const me = { name: 'あなたのなまえ' }
+const airi = {
+  // 文字列
+  name: "あいり",
+  // 数字
+  age: 22,
+  // 配列
+  hobbies: ["ゲーム", "映画", "ねぐせ。"],
+  // 真偽値
+  isHappy: true,
+  // オブジェクト
+  address: {
+    country: "日本",
+    city: "大阪",
+  },
+}
+const me = { name: "あいり" }
 
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
 
-// console.log(me)
+console.log(me)
 
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+const hikakin = {
+  name: "ヒカキン",
+  age: 24,
+  sayHello: function () {
+    console.log("ブンブンハロー YouTube")
+  },
+}
